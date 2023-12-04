a2e59fcd97809b16d4264e5e74a1139a9095aef9
hilal dedek <hdedek03@gmail.com>
2023-12-04 18:15:46 +0300

    code testing with postman

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 print("hello world")
-from flask import Flask, render_template, request, url_for
+from flask import Flask, jsonify, render_template, request, url_for
+import pymongo
 
 
 app = Flask(__name__)
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["mydatabase"]
 
 
 @app.route("/")
@@ -23,6 +28,19 @@ def login():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    if request.method == "POST":
+        req_Json = request.json
+        username = req_Json["username"]
+        email = req_Json["email"]
+        password = req_Json["password"]
+        return jsonify({"response": "welcome" + username + email + password})
+
+    else:
+        return jsonify({"response": "get request called"})
 
 
 @app.route("/post/<url>")
